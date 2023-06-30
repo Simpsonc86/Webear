582fa7d8ec4e7e5fe623276e86ba4b63c472b972
josh <ut1jschenk@gmail.com>
2023-06-30 16:47:40 -0500

    successfully seeded locally

---
+++ app/models/transactions.py
@@ -0,0 +1,31 @@
+from .db import db, environment, SCHEMA
+
+class Transaction(db.Model):
+    __tablename__ = 'transactions'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable = False)
+    transaction_type = db.Column(db.String, nullable = False)
+    share_price = db.Column(db.String, nullable = False)
+    shares_moved = db.Column(db.String, nullable = False)
+    stock_id = db.Column(db.Integer, db.ForeignKey("stocks.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    #relationship attributes
+    stock = db.relationship("Stock", back_populates="transactions")
+    user = db.relationship("User", back_populates="transactions")
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "date": self.date,
+            "transaction_type": self.transaction_type,
+            "share_price": self.share_price,
+            "shares_moved": self.shares_moved,
+            "user_id": self.user_id,
+            "stock_id": self.stock_id
+        }
---
+++ app/seeds/stocks.py
@@ -0,0 +1,27 @@
+from app.models import db, User, environment, SCHEMA
+import datetime
+from ..models import Stock
+from sqlalchemy.sql import text
+
+def seed_stocks():
+
+    alphabet = Stock(
+        base_price='122.22', company_name='Alphabet', ticker_symbol='GOOGL', available_shares=100000, total_shares = 1000000, created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
+    tesla = Stock(
+        base_price='272.22', company_name='Telsa', ticker_symbol='TSLA', available_shares=10000, total_shares = 100000, created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
+    berkshire = Stock(
+        base_price='822.22', company_name='Berkshire', ticker_symbol='BKSH', available_shares=200000, total_shares = 2000000, created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
+
+    db.session.add(alphabet)
+    db.session.add(tesla)
+    db.session.add(berkshire)
+    db.session.commit()
+
+
+def undo_stocks():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.stocks RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM stocks"))
+
+    db.session.commit()
