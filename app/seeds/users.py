7adcbcbfb0de0c8e00dd0cf02cffd317881557d1
michaelluutrong <michaelluutrong@gmail.com>
2023-07-03 13:21:49 -0700

    user seeders completed

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,15 +5,29 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password', balance = 1000000.01)
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password', balance = 20000.02)
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password', balance = 30000.03)
+        username='demo', email='demo@aa.io', password='demopassword', balance = 29359.48)
+    justin = User(
+        username='justin', email='justint@aa.io', password='justinpassword', balance = 36661.97)
+    johnny = User(
+        username='johnny', email='johnd@aa.io', password='johnnypassword', balance = 4414.57)
+    jennifer = User(
+        username='jennifer', email ='jlo@aa.io', password='jenniferpassword', balance=97140.52)
+    tom = User(
+        username='tom', email ='tomcruise@aa.io', password='tompassword', balance=11620.82)
+    morgan = User(
+        username='morgan', email ='mfreeman@aa.io', password='morganpassword', balance=88825.54)
+    brad = User(
+        username='brad', email ='bpitt@aa.io', password='bradpassword', balance=15359.80)
+    jackie = User(
+        username='jackie', email ='jchan@aa.io', password='jackiepassword', balance=43950.25)
+    nicolas = User(
+        username='nicolas', email ='ncage@aa.io', password='nicolaspassword', balance=93742.00)
+    taylor = User(
+        username='taylor', email ='tswift@aa.io', password='taylorpassword', balance=74902.78)
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    all_users = [demo, justin, johnny, jennifer, tom, morgan, brad, jackie, nicolas, taylor]
+
+    _ = [db.session.add(user) for user in all_users]
     db.session.commit()
 
 
