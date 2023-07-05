0d0406cb828a9767f20495815f28c37a44185878
Christopher Simpson <simpsonc6@winthropalumni.com>
2023-07-05 09:01:06 -0400

    filter resets on click and picking a stock

--- react-app/src/components/StockSearchFilter/index.js
+++ react-app/src/components/StockSearchFilter/index.js
@@ -12,7 +12,8 @@ export default function StockSearchFilter() {
     const stocks = useSelector((state) => (state.stocks.stocks ? Object.values(state.stocks.stocks) : []));
     // console.log("Type of stocks is: ",typeof stocks);
     // console.log("These are the stocks from the store--->", stocks);
-    const [searchList, setSearchList] = useState(stocks)
+    const [searchList, setSearchList] = useState([])
+    // const [filterQuery, setFilterQuery] = useState("")
     const stockList = [];
     
     stocks.forEach((stock)=>{
@@ -25,26 +26,41 @@ export default function StockSearchFilter() {
     }, [dispatch,]);
     
     const filterSearch = (e) => {
-        const query = e.target.value;
+        e.preventDefault()
+        const query = e.target.value
         // let newList = [...stockList]
         
         // console.log("This is the value of query",query);
             const newList = stockList.filter((stock) => stock.company_name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
         // console.log("this is the filtered list", newList);
-        
+        // if(!newList.length){
+        // return(<div className="search">No results found</div>)}
         if (!query.length){
             setSearchList([]);
-        }else setSearchList(newList)
-      
+        }else {
+            setSearchList(newList)
+        }
+
+        
+
+        // onclick=setSearchList([])
+        
     }
+    const navigateToStock=(e)=>{
+        setSearchList([])
+        e.target.value=''
+        // setFilterQuery('')
+        
+    }
+
     // console.log("USE STATE STOCKS",searchList);
     
     return (
         <div className='search-filter'>
-            <input className='search-field' onChange={filterSearch} placeholder='Search for a Company'></input>
+            <input className='search-field' onChange={filterSearch} onClick={navigateToStock} placeholder='Search for a Company'></input>
             <div className="search-list-stock">
                 {searchList.map((stock, index) => (
-                    <NavLink  className="search"key={index} to={`/`}>{stock.company_name}</NavLink>
+                    <NavLink  className="search"key={index} onClick={navigateToStock}to={`/stocks/${stock.id}`}>{stock.company_name}</NavLink>
                 ))}
             </div>           
         </div>
