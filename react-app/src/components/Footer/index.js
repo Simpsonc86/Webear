e0b858ec83e536cac94aad9afde2d64f54fae4b7
Christopher Simpson <simpsonc6@winthropalumni.com>
2023-08-10 16:03:55 -0400

    fixed footer links

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -6,20 +6,20 @@ export default function Footer() {
             <p>Disclaimer: This is not a real financial website with real-time stock prices and purchases!</p>
             <div className="github-links">
                 Contributor Links:
-                <a className="group-link" href="https://github.com/Simpsonc86">
+                <a className="group-link" href="https://github.com/Simpsonc86" target="_blank" rel="noopener noreferrer">
                     <img className="github-icon" src="/Github-icon.png" alt="github-icon" />
                     <p className="contributor-name">Christopher Simpson</p>
                 </a>
 
-                <a className="group-link" href="https://github.com/joshschenk">
+                <a className="group-link" href="https://github.com/joshschenk" target="_blank" rel="noopener noreferrer">
                     <img className="github-icon" src="/Github-icon.png" alt="github-icon" />
                     <p className="contributor-name">Joshua Schenk</p>
                 </a>
-                <a className="group-link" href="https://github.com/MichaelLuuTrong">
+                <a className="group-link" href="https://github.com/MichaelLuuTrong" target="_blank" rel="noopener noreferrer">
                     <img className="github-icon" src="/Github-icon.png" alt="github-icon" />
                     <p className="contributor-name">Michael Luu-Trong</p>
                 </a>
-                <a className="group-link" href="https://github.com/ChrisBeaman11">
+                <a className="group-link" href="https://github.com/ChrisBeaman11" target="_blank" rel="noopener noreferrer">
                     <img className="github-icon" src="/Github-icon.png" alt="github-icon" />
                     <p className="contributor-name">Christopher Beaman</p>
                 </a>               
